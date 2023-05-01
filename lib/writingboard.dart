5d96d1659a6129b73f128f5797e5d1f3be149347
Africanboy Kiima <fadhilkiima@gmail.com>
2023-05-01 16:08:34 +0100

    Talked about, streams,futures repeatedly to have a deep understanding of these beautiful things

--- lib/writingboard.dart
+++ lib/writingboard.dart
@@ -1,58 +1,94 @@
-import 'main.dart';
-void testinglfAndElseifWithElse(){
-  const age = 45;
-  //Returns older if one is older than or equal to 18
-  //And younger if one is less than 18
-  if (age >= 18){
-    print('You are older than 18 and now old enought to start making some choices ');
-  }else if (age < 18 ){
-    print("You aren't old enought to be alone, we are still holding your hand so don't worry");
-  }
-}
-
-//Create class person(model) that defines the attributes and methods of an object 
-/*Classes define the attributes and methods of an object
-The attributes being the characteristics of the object while the methods being the actions that can
-be performed on the objet or being the actions that the object performs
-. As seen below we've got a number of methods.
+/*A class is a model,the latter is an informative representation of an object,system or person
+In OOP, we have a number of stuff that we can define such as classes, objects,instances,propeties/attributes
+or fields, methods,
 */
-class Person{
-  //Define class attributes
+import 'dart:io';
+
+class Person {
+  //Define fields first
   String name;
   String surname;
-  int age;
   String address;
+  int age;
 
-  //Create constructor method to create the attributes of our object
-  Person(this.name, this.surname, this.address,this.age);
+  //Use constructor to assign defined fields to objects
+  Person(this.name, this.surname, this.address, this.age);
 
-  //Method to be performed on the object and make it run
-  void run(){
-    print("Running");
-  }
-
-  //
-  void breathe(){
-    print("Breathing");
-  }
-
-  void speaks(){
-    print("Speaking");
-  }
-
-  void see(){
-    print("Seeing");
+  String getName(val) {
+    val = stdin.readLineSync();
+    print("your name is $val");
+    return name;
   }
 }
-class Post{
-  /*The take away in dart is that when we are defining it's attributes, we have to first define its fields just
-  like one would define a variable, after we can then create a constructor to map or assign the attributes
-  of the object to the object each time it's instantiated
-  */
-  //Definition of the fields/properties/attributes
-  String title;
-  String content;
 
-  //Constructor to assign attributes to our objects
-  Post(this.title, this.content);
+/*Extensions are a great way of extending the functionality of a class, normally, we use extensions to */
+extension Run on Person {
+  void run() {
+    print("Runninng");
+  }
+}
+
+class Board {
+  String surface;
+  Board(this.surface);
+}
+
+//Asynchronous programming
+Future<String> getFullName(firstName, String lastName) {
+  return Future.delayed(const Duration(seconds: 10), () {
+    String cont = '$firstName $lastName';
+    return cont;
+  });
+}
+
+//Use of the future in a function
+/*The main take away here is that async and await are used together to harness the power of futures*/
+void test() async {
+  final resultsOfCalcution = await getFullName('Fadhil', 'Kiima');
+  print(resultsOfCalcution);
+}
+
+//Streams in dart : An asynchronous pipe of data
+Stream<String> me() {
+  return Stream.value('foo');
+}
+
+void strim() async {
+  await for (final value in me()) {
+    print(value);
+  }
+  print("Finished working");
+}
+
+Stream<String> getName() {
+  return Stream.periodic(const Duration(seconds: 3), (val) {
+    return "Hello World this is kiima from the democratic republic of Congo";
+  });
+}
+
+void testing() async {
+  await for (final val in getName()) {
+    print(val);
+  }
+}
+
+/*A stream is an asynchronous pipe of data.By asynchronous, I mean the ability for something to not
+do something at the same time or something doesn't happen at the same time so what happens with asynchronous
+programming is that we ask for a task to be done for us,however, it first takes some time before that task 
+is performed for us and results returned, the processing is done later and results are returned later.
+Streams and futures are the same in terms of their asynchronous capability except that with streams,when
+tasks begin, the data continues to be in a state of flow until the program in terminated for instance.
+With futures, we derive a mechanism that allows us to control the flow of data.However, data stops flowing
+as soon as results are returned 
+*/
+Stream<int> calculateProductOfTwoNumber() {
+  return Stream.periodic(const Duration(seconds: 1), (val) {
+    return 4 * 5;
+  });
+}
+
+void tester() async {
+  await for (final val in calculateProductOfTwoNumber()) {
+    print(val);
+  }
 }
